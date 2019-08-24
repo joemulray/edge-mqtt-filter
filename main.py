23952bfaf47c25198d68a378759fda4a758845e4
Joseph Mulray <joe.mulray@gmail.com>
2019-08-24 16:26:12 -0400

    add initial mqtt sub script

---
+++ main.py
@@ -0,0 +1,40 @@
+import paho.mqtt.client as mqttClient
+import time
+ 
+def on_connect(client, userdata, flags, rc):
+	if rc == 0:
+print("Connected to broker")
+	global Connected #Use global variable
+	Connected = True #Signal connection 
+	else:
+	print("Connection failed")
+ 
+def on_message(client, userdata, message):
+	print "Message received: "  + message.payload
+ 
+Connected = False #global variable for the state of the connection
+ 
+broker_address= "localhost" #Broker address
+port = 1883 #Broker port
+ 
+client = mqttClient.Client("Python")	   #create new instance
+client.on_connect= on_connect #attach function to callback
+client.on_message= on_message #attach function to callback
+ 
+client.connect(broker_address, port=port) #connect to broker
+ 
+client.loop_start()	#start the loop
+ 
+while Connected != True: #Wait for connection
+	time.sleep(0.1)
+ 
+client.subscribe("python/test")
+ 
+try:
+	while True:
+	time.sleep(1)
+
+except KeyboardInterrupt:
+	print "exiting"
+	client.disconnect()
+	client.loop_stop()
